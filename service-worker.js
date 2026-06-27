3589c61ea2e0cffc0a613f9c2742bf1e83094ce7
uptsapeacu-dot <uptsapeacu@gmail.com>
2026-06-27 14:37:12 -0300

    up

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,13 @@
-const CACHE_NAME = "chamada-upt-v1";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./service-worker.js",
-  "./assets/icon-192.png",
-  "./assets/icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+self.addEventListener('install', event => {
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => {
+    caches.keys().then(cacheNames => {
       return Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
+        cacheNames.map(cacheName => caches.delete(cacheName))
       );
-    })
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      return cachedResponse || fetch(event.request);
-    })
+    }).then(() => self.clients.claim())
   );
 });
